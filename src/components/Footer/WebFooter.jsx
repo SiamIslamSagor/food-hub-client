afd69e3338384f12cda6841c9460f7e6e0ee26c6
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-06 15:18:02 +0600

    added error page for footer all links

--- src/components/Footer/WebFooter.jsx
+++ src/components/Footer/WebFooter.jsx
@@ -24,38 +24,40 @@ const WebFooter = () => {
             <div>
               <Footer.Title title="Company" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">About</Footer.Link>
-                <Footer.Link href="#">Careers</Footer.Link>
-                <Footer.Link href="#">Success</Footer.Link>
-                <Footer.Link href="#">Mission</Footer.Link>
-                <Footer.Link href="#">Blog</Footer.Link>
+                <Footer.Link href="/error">About</Footer.Link>
+                <Footer.Link href="/error">Careers</Footer.Link>
+                <Footer.Link href="/error">Success</Footer.Link>
+                <Footer.Link href="/error">Mission</Footer.Link>
+                <Footer.Link href="/error">Blog</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
               <Footer.Title title="help center" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Discord Server</Footer.Link>
-                <Footer.Link href="#">Twitter</Footer.Link>
-                <Footer.Link href="#">Facebook</Footer.Link>
-                <Footer.Link href="#">Contact Us</Footer.Link>
+                <Footer.Link href="/error">Discord Server</Footer.Link>
+                <Footer.Link href="/error">Twitter</Footer.Link>
+                <Footer.Link href="/error">Facebook</Footer.Link>
+                <Footer.Link href="/error">Contact Us</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
               <Footer.Title title="legal" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Licensing</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+                <Footer.Link href="/error">Privacy Policy</Footer.Link>
+                <Footer.Link href="/error">Licensing</Footer.Link>
+                <Footer.Link href="/error">Terms &amp; Conditions</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
               <Footer.Title title="Contact" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Name: Md. Rahman</Footer.Link>
-                <Footer.Link href="#">Address: House #12, Road #34</Footer.Link>
-                <Footer.Link href="#">City: Dhaka</Footer.Link>
-                <Footer.Link href="#">Post Code: 1207</Footer.Link>
-                <Footer.Link href="#">Email: rahman@gmail.com</Footer.Link>
+                <Footer.Link href="/error">Name: Md. Rahman</Footer.Link>
+                <Footer.Link href="/error">
+                  Address: House #12, Road #34
+                </Footer.Link>
+                <Footer.Link href="/error">City: Dhaka</Footer.Link>
+                <Footer.Link href="/error">Post Code: 1207</Footer.Link>
+                <Footer.Link href="/error">Email: rahman@gmail.com</Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -63,16 +65,16 @@ const WebFooter = () => {
         <div className="w-full bg-gray-700">
           <div className=" container mx-auto  text-white px-4 py-6 sm:flex sm:items-center sm:justify-between">
             <Footer.Copyright
-              href="#"
+              href="/error"
               by=" FOODHUB. All Rights Reserved."
               year={2023}
             />
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-              <Footer.Icon href="#" icon={BsFacebook} />
-              <Footer.Icon href="#" icon={BsInstagram} />
-              <Footer.Icon href="#" icon={BsTwitter} />
-              <Footer.Icon href="#" icon={BsGithub} />
-              <Footer.Icon href="#" icon={BsDribbble} />
+              <Footer.Icon href="/error" icon={BsFacebook} />
+              <Footer.Icon href="/error" icon={BsInstagram} />
+              <Footer.Icon href="/error" icon={BsTwitter} />
+              <Footer.Icon href="/error" icon={BsGithub} />
+              <Footer.Icon href="/error" icon={BsDribbble} />
             </div>
           </div>
         </div>
